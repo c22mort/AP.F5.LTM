7c9eacb1bf8e8ab7b5ba8da3f48252fb4e12a0c1
Andrew Patrick <and.patrick@gmail.com>
2018-10-29 10:23:18 +0000

    Installer Fix

--- AP.F5.LTM.Discovery/Properties/AssemblyInfo.cs
+++ AP.F5.LTM.Discovery/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.10.3.0")]
-[assembly: AssemblyFileVersion("18.10.3.0")]
+[assembly: AssemblyVersion("18.10.7.1")]
+[assembly: AssemblyFileVersion("18.10.7.1")]
 // log4net Config File Location
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
